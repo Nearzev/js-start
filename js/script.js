c407d0ad943c214d4a505368fef261b3fd0a3c0e
Kushnirov Nikita <niki.cushnirov@yandex.rui>
2022-05-23 22:58:12 +0300

    modified first app

--- js/script.js
+++ js/script.js
@@ -9,10 +9,34 @@ const personalMovieDB = {
    privat: false,
 };
 
-const a = prompt('Один из последних просмотреных фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотреных фильмов?'),
-      d = prompt('На сколько оцените его?');
+// const a = prompt('Один из последних просмотреных фильмов?'),
+//       b = prompt('На сколько оцените его?'),
+//       c = prompt('Один из последних просмотреных фильмов?'),
+//       d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних просмотреных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
+
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('Dobra');
+   } else {
+      console.log('Bober perdole');
+      i--;
+   }
+}
+
+if (personalMovieDB.count < 10) {
+   console.log('Bober perdole');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('boder');
+} else if (personalMovieDB.count > 30) {
+   console.log('Dobra dobra');
+} else {
+   console.log('o kurva');
+}
+console.log(personalMovieDB);
